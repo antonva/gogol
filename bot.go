6ab92e679e72b8efd378cd97ad1f668a9804fe76
Anton Vilhelm Ásgeirsson <anton@antonvilhelm.is>
2015-02-18 09:07:37 +0000

    refactored sed into its own function, now with RegEx(tm)

--- bot.go
+++ bot.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 	"strings"
 	"log"
+	"regexp"
+	"time"
 	"code.google.com/p/gcfg"
 	"github.com/thoj/go-ircevent"
 	"github.com/aarzilli/golua/lua"
@@ -17,23 +19,17 @@ func messageParser(e *irc.Event, conf Config, buffer *Msgbuf) string {
 	buffer.Append(nick, channel, msg)
 	if len(msg) > 1 {
 		if string(msg[0]) == conf.Bot.Trigger {
-			split := strings.Split(msg[1:], " ")
-			return split[0]
+			//split := strings.Split(msg[1:], " ")
+			//return split[0]
 	/*
 	 * Sed style replace on last line spoken
-	 * TODO: split into own function/plugin.
 	 * TODO: refactor so you can correct others.
 	 */
 		} else if string(msg[0:2]) == "s/" {
 			for i := len(buffer.Buffer)-2; i >= 0; i-- {
 				if nick == buffer.Buffer[i].Nick {
-					find_replace := strings.Split(msg[2:], "/")
-					if len(find_replace) > 1 {
-						return "I think you meant: " +
-						strings.Replace(buffer.Buffer[i].Message,
-						find_replace[0], find_replace[1], 1)
-					}
-					break
+					return "I think you meant: " +
+					sed(msg[2:], buffer.Buffer[i].Message)
 				}
 			}
 		}
@@ -41,6 +37,16 @@ func messageParser(e *irc.Event, conf Config, buffer *Msgbuf) string {
 	return ""
 }
 
+func sed(rgx string,msg string) string {
+	split_slice := strings.Split(rgx, "/")
+	if len(split_slice) > 1 {
+		//TODO: Make it work inline and not just for one off \/'s.
+		if split_slice[0] == "\\" { split_slice[0] = "/" }
+		re := regexp.MustCompile(split_slice[0])
+		return re.ReplaceAllString(msg, split_slice[1])
+	}
+	return ""
+}
 
 
 func main() {
@@ -81,7 +87,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	con.AddCallback("001", func(e *irc.Event) { con.Join("#neoplastic") })
+	con.AddCallback("001", func(e *irc.Event) {
+		con.Privmsg("nickserv", "identify pump19")
+		time.Sleep( 2 * time.Second )
+		con.Join("#frost")
+	})
 
 	con.AddCallback("PRIVMSG", func(e *irc.Event) {
 		s := messageParser(e, conf, &buffer)
