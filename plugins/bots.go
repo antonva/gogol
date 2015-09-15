a1ff9def3960b2c0425eb19b8b8c97a20a22675c
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2015-09-15 22:50:43 +0000

    added config.json, deprecated old init style config

--- plugins/bots.go
+++ plugins/bots.go
@@ -2,5 +2,5 @@ package plugins
 
 // Written mainly as an example plugins.
 func (p *Plugins) Bots(s []string) []string{
-    return []string{"Reporting in! [Go] Oedo Cyber Police."} 
+    return []string{"Reporting in! [Go] Oedo Cyber Police."}
 }
